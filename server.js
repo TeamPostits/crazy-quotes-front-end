7c491922486bee75e9120d02fe2d92f7235cf80a
andberg <andreaberglund@hotmail.com>
2015-04-14 12:06:02 +0200

    Added simple Node server and am trying to get it to work with tha Java back-end

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var server = express();
+
+var allowCrossDomain = function (req, res, next) {
+	req.header('Access-Control-Allow-Origin', '*');
+	req.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	req.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	if (req.method === 'OPTIONS') {
+		res.send(200);
+	} else {
+		next();
+	}
+};
+server.use(allowCrossDomain);
+
+server.use(express.static(__dirname + '/public'));
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get('*', function (req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+var port = 10001;
+
+server.listen(port, function () {
+	console.log('server listening on port ' + port);
+});
